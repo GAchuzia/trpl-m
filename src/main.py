27debba805fe90526259b9bf150f7ed0667425ee
Grant <achuziaduby@gmail.com>
2025-03-10 18:55:27 -0400

    Add title card for game

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
-from textual.app import App
+from textual.containers import Grid
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header, Digits, Label
 
 class TrplM(App):
 
-    def __init__(self):
-        pass
+    BINDINGS = [
+                ("q", "quit", "Quit Game"),
+                ]
+    CSS_PATH = "styles.tcss"
+    menu_options = ["PLAY", "VS", "SETTINGS", "EXIT"]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app"""
+        yield Header()
+        yield Footer()
+        yield Label("MENTAL MATH MONSTER", id="title")
+
+    
+    def quit_app(self) -> None:
+        """An action to quit the game"""
+        self.action_quit()
  
 
 if __name__ == "__main__":
-    pass
+    app = TrplM()
+    app.run()
