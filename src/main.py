ff6e88e9027af7118e87317182d0f9843910ad2e
Grant <achuziaduby@gmail.com>
2025-03-12 05:17:07 -0400

    feat: Add main menu and outline for settings menu
    - Removed .tccs file to focus on using the Rich library

--- src/main.py
+++ src/main.py
@@ -1,27 +1,94 @@
-from textual.containers import Grid
-from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Digits, Label
+import time
+from rich.panel import Panel
+from rich.console import Console
 
-class TrplM(App):
+console = Console()
 
-    BINDINGS = [
-                ("q", "quit", "Quit Game"),
-                ]
-    CSS_PATH = "styles.tcss"
-    menu_options = ["PLAY", "VS", "SETTINGS", "EXIT"]
-
-    def compose(self) -> ComposeResult:
-        """Create child widgets for the app"""
-        yield Header()
-        yield Footer()
-        yield Label("MENTAL MATH MONSTER", id="title")
-
+class Trplm:
+    """Main Game Class"""
     
-    def quit_app(self) -> None:
-        """An action to quit the game"""
-        self.action_quit()
- 
+    def __init__(self):
+        self.menu = Menu()
+        self.settings_menu = SettingsMenu()
+        self.game_loop()
+
+    def game_loop(self) -> None:
+        """Main game loop"""
+        self.menu.display_menu()
+        user_input = int(input("Enter: "))
+        while user_input != 3:
+            if user_input == 1:
+                self.play_game()
+            elif user_input == 2:
+                self.settings_menu.display_menu()
+
+            self.menu.display_menu()
+            try:
+                user_input = int(input("Enter: "))
+            except ValueError:
+                console.print("ERROR: ENTER VALID NUMBER", style="bold red")
+                user_input = 0
+
+        console.print("Till next time!", style="bold red")
+        time.sleep(0.6)
+        console.clear()
+
+    def play_game(self):
+        """Play the game"""
+        console.clear()
+        console.print(Panel("MENTAL MATH MONSTER", style="bold red"))
+
+    def auto_answer(self, correct_answer):
+        """Auto answer (not yet implemented)"""
+        pass
+
+
+class Menu:
+    """Main Menu"""
+    
+    def __init__(self):
+        self.title = "MENTAL MATH MONSTER"
+        self.menu = "(1) PLAY\n(2) SETTINGS\n(3) EXIT"
+    
+    def display_menu(self) -> None:
+        """Display the main menu"""
+        console.clear()
+        console.print(Panel(self.menu, title=self.title, style="bold red", title_align="left"))
+
+
+class SettingsMenu:
+    """Settings Menu"""
+    
+    def __init__(self):
+        self.menu = "SETTINGS\n(1) OPERATIONS\n(2) TIMER\n(3) QUESTION COUNT/TYPE\n(4) MAIN MENU"
+        self.Menu = Menu()
+    def display_menu(self) -> None:
+        console.clear()
+        console.print(Panel(self.menu, title="MENTAL MATH MONSTER", style="bold red", title_align="left"))
+
+        user_input = int(input("Enter: "))
+        while user_input != 4:
+            if user_input == 1:
+                pass
+               
+            elif user_input == 2:
+                pass
+   
+            elif user_input == 3:
+                pass              
+            try:
+                user_input = int(input("Enter: "))
+                console.print(Panel(renderable="WIP", title="TIMER", style="bold red", title_align="left"))
+            except ValueError:
+                console.print("ERROR: ENTER VALID NUMBER", style="bold red")
+                user_input = 0
+
+        
+
 
 if __name__ == "__main__":
-    app = TrplM()
-    app.run()
+    try:
+        Trplm()
+    except KeyboardInterrupt:
+        console.clear()
+        console.print("Till Next Time.", style="red")
