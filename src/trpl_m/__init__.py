034e8521eb978d95ef7859c9c6056957d48a8f40
Grant <grant.achuzia@gmail.com>
2025-06-24 23:27:22 -0400

    fix: update project structure

--- src/trpl_m/__init__.py
+++
@@ -1,4 +0,0 @@
-from .main import Trplm 
-
-__version__ = "1.0.0"
-__author__ = "Grant Achuzia"
---
+++ src/trplm/__main__.py
@@ -0,0 +1,9 @@
+"""
+src/trplm/__main__.py
+Description: Main entry point for the trplm tool
+"""
+
+from trplm.cli import main
+
+if __name__ == "__main__":
+    main()
