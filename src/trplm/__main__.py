ba1043d280e4c70dd7025f23b082da9b08d764ba
Grant <grant.achuzia@gmail.com>
2025-08-03 21:33:04 -0400

    chore: test pre-commit

--- src/trplm/__main__.py
+++ src/trplm/__main__.py
@@ -3,7 +3,13 @@ src/trplm/__main__.py
 Description: Main entry point for the trplm tool
 """
 
-from trplm.cli import main
+from trplm.trplm import main
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
